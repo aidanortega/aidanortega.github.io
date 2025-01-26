dc099c2e708683ccad5a374f943bd0386ccad988
aidanortega <33027075+aidanortega@users.noreply.github.com>
2025-01-26 15:50:57 +0000

    Deploying to gh-pages from @ aidanortega/aidanortega.github.io@dbd5726c3f1a3e67b2b20d530ac0074c404dd685 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -429,14 +429,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%69%64%61%6E.%6F%72%74%65%67%61@%69%66%63.%75%6E%61%6D.%6D%78", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://orcid.org/0000-0002-5487-0592", "_blank");
+        },
+      },{
+        id: 'social-researchgate',
+        title: 'ResearchGate',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.researchgate.net/profile/https://www.researchgate.net/profile/Aidan-Ortega/", "_blank");
         },
       },{
         id: 'social-rss',
@@ -450,14 +457,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=gm4HmO0AAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
